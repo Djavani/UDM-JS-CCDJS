7d26d5561f5b906334e4e5fd6886b2ef1c64cdfc
Djavani Gomes <djavanirgomes@gmail.com>
2018-11-15 13:22:29 -0200

    instalando o EJS

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'ejs');
+
+app.get('/tecnologia', function(req, res){
+    //res.render("secao/tecnologia.ejs")
+    res.render("secao/tecnologia")
+});
+
 app.get('/', function(req, res){
     res.send("<html><body>Portal de noticias do bacana vindo do express</body></html>")
 });
 
-app.get('/tecnologia', function(req, res){
-    res.send("<html><body>Noticias de tecnologia vindo do express</body></html>")
-});
 
 
 app.listen(3000, function(){
