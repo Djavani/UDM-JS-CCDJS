260b07c3d312ca378ea4913b338b1029aae730bf
Djavani Gomes <djavanirgomes@gmail.com>
2018-11-15 21:53:51 -0200

    refactoring do projeto adicionado modulo de rotas Cosign

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 var app = require('./config/server');
 
-var rotaNoticias = require('./app/routes/noticias');
-rotaNoticias(app);
+//antes de usar o modulo de rotas Consign
+// var rotaNoticias = require('./app/routes/noticias');
+// rotaNoticias(app);
+// var rotaHome = require('./app/routes/home')(app);
+// var rotaFormInclusaoNoticia = require('./app/routes/formulario_inclusao_noticia')(app);
 
-var rotaHome = require('./app/routes/home')(app);
 
-var rotaFormInclusaoNoticia = require('./app/routes/formulario_inclusao_noticia')(app);
 
 app.listen(3000, function(){
     console.log('servidor ON!');
