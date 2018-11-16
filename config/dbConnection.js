a022e8b87c8cfd7cc06529017e826230247bfe27
Djavani Gomes <djavanirgomes@gmail.com>
2018-11-15 22:28:16 -0200

    Refactoring do projeto prático portal de notícias com Consign (banco de dados)

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,10 +1,18 @@
 var mysql = require('mysql');
 
-module.exports = function () {
+var connMySQL = function () {
+    // só vai ser executado quando a pagina que utiliza o BD for chamada e nao no carregamento do programa
+    console.log('Conexao com o bd foi estabelecida');
     return mysql.createConnection({
         host: 'localhost',
         user: 'root',
         password: 'admin',
         database: 'portal_noticias'
     });
+
+}
+
+module.exports = function () {
+    console.log('O autoload carregou o modulo de conexao com o BD.');    
+    return connMySQL;
 }
