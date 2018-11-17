86581f4e33a1b22765368b2d2b5cb3633453e0d3
Djavani Gomes <djavanirgomes@gmail.com>
2018-11-16 22:01:36 -0200

    Refactoring do projeto prático portal de notícias - implementando classes

--- app/routes/noticia.js
+++ app/routes/noticia.js
@@ -2,10 +2,10 @@ module.exports = function (application) {
     
     application.get('/noticia', function (req, res) {
         var connection = application.config.dbConnection();
-        var noticiasModel = application.app.models.noticiasModel;
+        var noticiasModel = new application.app.models.NoticiasDAO(connection);
 
-        noticiasModel.getNoticia(connection, function (error, result) {
-            res.render("noticias/noticia", {noticia : result});
+        noticiasModel.getNoticia(function (error, result) {
+            res.render("noticias/noticia", {noticias : result});
         });
     });
 };
--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,7 +2,6 @@ var mysql = require('mysql');
 
 var connMySQL = function () {
     // só vai ser executado quando a pagina que utiliza o BD for chamada e nao no carregamento do programa
-    console.log('Conexao com o bd foi estabelecida');
     return mysql.createConnection({
         host: 'localhost',
         user: 'root',
@@ -13,6 +12,5 @@ var connMySQL = function () {
 }
 
 module.exports = function () {
-    console.log('O autoload carregou o modulo de conexao com o BD.');    
     return connMySQL;
 }
